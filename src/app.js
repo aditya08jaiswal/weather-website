972cc54c543aeb5aa1a735e5787fbff4a6b25351
iam844 <jaiswaladitya844@gmail.com>
2020-03-22 02:31:38 +0530

    Build a JSON HTTP Endpoint

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -53,13 +55,29 @@ app.get('/weather', (req, res) => {
         })
     }
 
-    console.log(req.query.address)
+    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return res.send({ error })
+        }
 
-    res.send({
-        forecast: 'It is snowing',
-        location: 'Surat',
-        address: req.query.address
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({error})
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
     })
+
+    // res.send({
+    //     forecast: 'It is snowing',
+    //     location: 'Surat',
+    //     address: req.query.address
+    // })
 })
 
 app.get('/products', (req, res) => {
